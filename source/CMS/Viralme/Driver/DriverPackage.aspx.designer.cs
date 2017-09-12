9226b31ddf953fb6349df1bcb04647512dcd87aa
packnezhad <packnezhad@gmail.com>
2017-09-12 12:47:14 +0330

    jsgrid is added

--- source/CMS/Viralme/Driver/DriverPackage.aspx.designer.cs
+++ source/CMS/Viralme/Driver/DriverPackage.aspx.designer.cs
@@ -47,14 +47,5 @@ namespace CMS.Viralme.Driver {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gridShowPrice;
-        
-        /// <summary>
-        /// txt_location control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txt_location;
     }
 }
